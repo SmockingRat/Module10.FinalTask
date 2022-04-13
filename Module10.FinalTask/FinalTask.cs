dc5062bd6b737a44dc0001fd2bce0f0145c8da32
SmokingRat <halfbrikbarry@gmail.com>
2022-04-13 22:12:12 +0300

    Добавьте файлы проекта.

---
+++ Module10.FinalTask/FinalTask.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Module10.FinalTask
+{
+    class FinalTask
+    {
+
+        static ILogger Logger { get; set; }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите число 1");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите число 2");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                MyMath math = new MyMath();
+
+                Logger = new Logger();
+
+                var Work1 = new Worker(Logger);
+                var Work2 = new Worker2(Logger);
+
+                Work1.Work();
+                math.PlusNumbers(a, b);
+                Work2.Work();   
+            }
+            catch
+            {
+                Logger = new Logger();
+                var err = new ErrorWorker(Logger);
+                err.Work();
+                
+            }
+            Console.ReadKey();
+        }
+
+        interface IPlus
+        {
+            void PlusNumbers(int a, int b);
+        }
+
+        public class MyMath : IPlus
+        {
+            public void PlusNumbers(int a, int b)
+            {
+                Console.WriteLine("Полученное число: {0}", a+b);
+            }
+
+
+
+        }
+
+
+    }
+
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+    }
+
+
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Event(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public interface IWorker
+    {
+        void Work();
+    }
+
+}
---
+++ Module10.FinalTask/Worker.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10.FinalTask
+{
+    public class Worker: IWorker
+    {
+
+        public ILogger Logger { get; }
+
+        public Worker(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Work()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Logger.Event("Происходит вычисление");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        
+    }
+
+    public class Worker2 : IWorker
+    {
+
+        ILogger Logger { get; }
+
+        public Worker2(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Work()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Logger.Event("Вычисление завершено");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+
+    public class ErrorWorker : IWorker
+    {
+
+        public ILogger Logger { get; }
+
+        public ErrorWorker(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Work()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Logger.Error("Вы не ввели требуемые значения!");
+        }
+
+
+    }
+
+
+}
